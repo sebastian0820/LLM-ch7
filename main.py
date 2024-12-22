048a2c98c1faaa23bd674079732081e775ea67bd
sebastian0820 <sebastian.plata.dev@gmail.com>
2024-12-22 13:11:03 -0300

    fixed

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def download_and_load_file(file_path, url):
         data = json.load(file)
     return data
 
-file_path = "instruction-data.json"
+file_path = "jjj.json"
 url = (
     "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
     "/main/ch07/01_main-chapter-code/instruction-data.json"
@@ -38,7 +38,7 @@ train_portion = int(len(data) * 0.85)
 test_portion = int(len(data) * 0.1)
 val_portion = len(data) - train_portion - test_portion
 train_data = data[:train_portion]
-test_data = data[train_portion:train_portion + test_portion]
+test_data = data
 val_data = data[train_portion + test_portion:]
 # print("Training set length:", len(train_data))
 # print("Test set length:", len(test_data))
@@ -89,25 +89,25 @@ customized_collate_fn = partial(
     allowed_max_length = 1024
 )
 
-train_dataset = InstructionDataset(train_data, tokenizer)
-train_loader = DataLoader(
-    train_dataset,
-    batch_size = batch_size,
-    collate_fn = customized_collate_fn,
-    shuffle = True,
-    drop_last = True,
-    num_workers = num_workers
-)
-val_dataset = InstructionDataset(val_data, tokenizer)
-val_loader = DataLoader(
-    val_dataset,
-    batch_size=batch_size,
-    collate_fn=customized_collate_fn,
-    shuffle=False,
-    drop_last=False,
-    num_workers=num_workers
-)
-test_dataset = InstructionDataset(test_data, tokenizer)
+# train_dataset = InstructionDataset(train_data, tokenizer)
+# train_loader = DataLoader(
+#     train_dataset,
+#     batch_size = batch_size,
+#     collate_fn = customized_collate_fn,
+#     shuffle = True,
+#     drop_last = True,
+#     num_workers = num_workers
+# )
+# val_dataset = InstructionDataset(val_data, tokenizer)
+# val_loader = DataLoader(
+#     val_dataset,
+#     batch_size=batch_size,
+#     collate_fn=customized_collate_fn,
+#     shuffle=False,
+#     drop_last=False,
+#     num_workers=num_workers
+# )
+test_dataset = InstructionDataset(data, tokenizer)
 test_loader = DataLoader(
     test_dataset,
     batch_size=batch_size,
@@ -139,12 +139,18 @@ model_configs = {
 CHOOSE_MODEL = "gpt2-small (124M)"
 BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
 model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
-settings, params = download_and_load_gpt2(
-    model_size=model_size,
-    models_dir="gpt2"
-)
+# settings, params = download_and_load_gpt2(
+#     model_size=model_size,
+#     models_dir="gpt2"
+# )
+
 model = GPTModel(BASE_CONFIG)
-load_weights_into_gpt(model, params)
+# load_weights_into_gpt(model, params)
+import re
+file_name = f"{re.sub(r'[ ()]', '', CHOOSE_MODEL) }-sft.pth"
+model.load_state_dict(torch.load(file_name))
+print(f"Model loaded from {file_name}")
+
 model.eval()
 
 from chapter05 import generate, text_to_token_ids, token_ids_to_text, calc_loss_loader, train_model_simple
@@ -168,23 +174,23 @@ from chapter05 import generate, text_to_token_ids, token_ids_to_text, calc_loss_
 # print("Training loss:", train_loss) 
 # print("Validation loss:", val_loss)
 
-import time
-start_time = time.time()
-torch.manual_seed(123)
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.00005, weight_decay=0.1)
-num_epochs = 2
-train_losses, val_losses, tokens_seen = train_model_simple(model, train_loader, val_loader, optimizer, device,
-                                                            num_epochs=num_epochs, eval_freq=5, eval_iter=5,
-                                                            start_context=format_input(val_data[0]), tokenizer=tokenizer)
-end_time = time.time()
-execution_time_minutes = (end_time - start_time) / 60
-print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+# import time
+# start_time = time.time()
+# torch.manual_seed(123)
+# optimizer = torch.optim.AdamW(model.parameters(), lr=0.00005, weight_decay=0.1)
+# num_epochs = 2
+# train_losses, val_losses, tokens_seen = train_model_simple(model, train_loader, val_loader, optimizer, device,
+#                                                             num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+#                                                             start_context=format_input(val_data[0]), tokenizer=tokenizer)
+# end_time = time.time()
+# execution_time_minutes = (end_time - start_time) / 60
+# print(f"Training completed in {execution_time_minutes:.2f} minutes.")
 
-from chapter05 import plot_losses
-epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
-plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+# from chapter05 import plot_losses
+# epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+# plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
 
-for entry in test_data[:3]:
+for entry in data:
     input_text = format_input(entry)
     token_ids = generate(
         model=model,
@@ -199,14 +205,12 @@ for entry in test_data[:3]:
         .replace("### Response:", "")
         .strip()
     )
-    with open("results.txt", "w", encoding="utf-8") as file:
-        file.write(input_text)
+    with open("results.txt", "a", encoding="utf-8") as file:
         file.write(input_text)
         file.write(f"\nCorrect response:\n>> {entry['output']}")
         file.write(f"\nModel response:\n>> {response_text.strip()}")
-        file.write("-------------------------------------")
-
-import re
-file_name = f"{re.sub(r'[ ()]', '', CHOOSE_MODEL) }-sft.pth"
-torch.save(model.state_dict(), file_name)
-print(f"Model saved as {file_name}")
+        file.write("\n-------------------------------------\n")
+print("Results saved to results.txt")
+# file_name = f"{re.sub(r'[ ()]', '', CHOOSE_MODEL) }-sft.pth"
+# torch.save(model.state_dict(), file_name)
+# print(f"Model saved as {file_name}")
